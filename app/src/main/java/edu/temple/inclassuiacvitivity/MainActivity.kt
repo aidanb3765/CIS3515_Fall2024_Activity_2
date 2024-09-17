cdd0d556a459e232a409cdac9763d00665cc78cd
aidanb3765 <aidan.blaney@temple.edu>
2024-09-17 12:02:06 -0400

    Update to For loop, Step4
    Changed array list to a for loop with 5 to 100; added listener for the selection of sizing

--- app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.*
+import com.google.android.material.navigation.NavigationBarView.OnItemSelectedListener
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,7 +16,8 @@ class MainActivity : AppCompatActivity() {
 
         /* TODO Step 2: Populate this array with multiples of 5 from 5 - 100*/
         //val numberArray = Array Of Numbers
-        val numberArray = arrayListOf(5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100)
+        val numberArray = arrayListOf<Int>()
+            for(i in 5..100 step 5) {numberArray.add(i)}
 
         /* TODO Step 3: Create adapter to display items from array in Spinner */
         //spinner.adapter = ArrayAdapter...
@@ -22,6 +25,14 @@ class MainActivity : AppCompatActivity() {
 
         // TODO Step 4: Change TextView's text size to the number selected in the Spinner */
         //spinner.onItemSelectedListener = object: ...
+        spinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
+            override fun onItemSelectected(p0:AdapterView<*>?, p1:View?, p2:Int, p3:Long){
+                val number = numberArray[p2]
+                displayTextView.textSize = number.toFloat()
+            }
 
+            override fun onNothingSelected(p0:AdapterView<*>?){}
+
+        }
     }
 }
